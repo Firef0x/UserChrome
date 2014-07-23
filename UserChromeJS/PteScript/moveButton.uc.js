5ae103d4e7f1ab97ef1911908e912371d482ae14
Firef0x <Firefgx@gmail.com>
2014-07-23 09:23:37 +0800

    更新moveButton和SaveUserChromeJS，增加IME-Colors 1. 从 https://github.com/ywzhaiqi/userChromeJS/blob/master/moveButton/moveButton.uc.js    处更新 moveButton.uc.js 版本为 2014.07.03    该版本的提交为 https://github.com/ywzhaiqi/userChromeJS/commit/eea138c50b8535eb423c4defb700ed6737cb0d76 2. 从 https://github.com/ywzhaiqi/userChromeJS/blob/master/SaveUserChromeJS/SaveUserChromeJS.uc.js    处更新 SaveUserChromeJS.uc.js 版本为 0.4    该版本的提交为 https://github.com/ywzhaiqi/userChromeJS/commit/fa9bb841d0ee29fb4ccd8b2b2788d1fe63af2be1 3. 从 https://github.com/ywzhaiqi/userChromeJS/blob/master/IME-Colors.uc.js    处增加 IME-Colors.uc.js 版本为 0.0.9    该版本的提交为 https://github.com/ywzhaiqi/userChromeJS/commit/134633954d353eb233962b43b2df0c314f1864e1

--- UserChromeJS/PteScript/moveButton.uc.js
+++ UserChromeJS/PteScript/moveButton.uc.js
@@ -5,9 +5,10 @@
 // @namespace      ywzhaiqi@gmail.com
 // @include        main
 // @charset        UTF-8
-// @version        v2014.05.05
+// @version        2014.07.03
 // @homepageURL    https://github.com/ywzhaiqi/userChromeJS/tree/master/moveButton
 // @reviewURL      http://bbs.kafan.cn/thread-1572303-1-1.html
+// @note           2014/06/03 增加对按钮或菜单属性的设置，修正 bar 必须设置 pos 才会生效的问题。
 // @note           2013/06/03 ver0.0.3  改进一些情况下无法移动的问题。
 // @note           2013/05/22 ver0.0.2，新增参数 clone: true（克隆按钮/菜单，原来的保留）
 // @note           2013/05/21 初始版本
@@ -46,92 +47,179 @@
 
 location == "chrome://browser/content/browser.xul" && (function(){
 
-var moveButton = {
-    buttons:[
-        { id: "greasemonkey-tbb", bar: "TabsToolbar_aidBar", pos: 1 },//greasemonkey扩展
-		{ id: "stylish-toolbar-button", bar: "TabsToolbar_aidBar", pos: 2 },//stylish 扩展
-		//{ id: "abp-toolbarbutton", bar: "TabsToolbar_aidBar", pos: 3 },// ABP、ABE扩展
+    var buttons = [
+		{ id: "greasemonkey-tbb", bar: "TabsToolbar_aidBar", pos: 1 },//greasemonkey扩展
+		 { id: "stylish-toolbar-button", bar: "TabsToolbar_aidBar", pos: 2 },//stylish 扩展
+		// { id: "abp-toolbarbutton", bar: "TabsToolbar_aidBar", pos: 3 },// ABP、ABE扩展
 		{ id: "lpt_lastpass-compact-btn", bar: "TabsToolbar_aidBar", pos: 4 },// Lastpass 扩展
 		{ id: "wot-button", bar: "TabsToolbar_aidBar", pos: 5 },// WOT 扩展
 		{ id: "pan-toolbarbutton", bar: "TabsToolbar_aidBar", pos: 6 },// AutoProxy 扩展
 		{ id: "RIL_toolbar_button", bar: "TabsToolbar_aidBar", pos: 7 },// Pocket 扩展
-		{ id: "sessionmanager-undo", bar: "TabsToolbar_aidBar", pos: 8 },// SessionManager 扩展
-		//{ id: "sessionmanager-toolbar", bar: "TabsToolbar_aidBar", pos: 9 },// SessionManager 扩展
+		// { id: "sessionmanager-undo", bar: "TabsToolbar_aidBar", pos: 8 },// SessionManager 扩展
+		// { id: "sessionmanager-toolbar", bar: "TabsToolbar_aidBar", pos: 9 },// SessionManager 扩展
 		{ id: "firebug-badged-button", bar: "TabsToolbar_aidBar", pos: 10 },// Firebug 扩展
-		{ id: "redisposition-button", bar: "TabsToolbar_aidBar", pos: 11 },// Firebug 扩展
-    ],
+		{ id: "redisposition-button", bar: "TabsToolbar_aidBar", pos: 11 },// Redisposition 扩展
+		{ id: "redirector-icon", bar: "TabsToolbar_aidBar", pos: 12 },// Redirector.uc.js 脚本
 
-    interval: 200, // 0.2秒间隔
-    maxcount: 100, // 最大100回，至少 interval * maxcount 秒
-    count: 0,
-    timer: null,
+        // { id: "autoReaderButton", bar: "PersonalToolbar", pos: 1 },
+        // { id: "showFlagS-icon", insertBefore: "bookmarks-menu-button" },
+        // { id: "nosquint-status", bar: "dactyl-status-bar" },
+        // { id: "uAutoPagerize-icon", bar: "dactyl-status-bar" },
+        // { id: "SimpleMusicPlayer", insertAfter: "tabcinema-button" },
+        // { id: "redirector-icon", bar: "dactyl-addon-bar" },
+        // { id: "statusbar-translator", bar: "PersonalToolbar" },
+        // { id: "userChromebtnMenu", bar: "dactyl-addon-bar" },
+        // { id: "PanelUI-menu-button", bar: "PersonalToolbar" },
 
-    init: function(){
-        this.delayRun();
+        // // 改 Greasemonkey 按钮为右键弹出菜单
+        // {id: "greasemonkey-tbb",
+        //     attr: {
+        //         type: "",
+        //         context: "_child"
+        //     }
+        // },
 
-        window.addEventListener('aftercustomization', this.delayRun.bind(this), false);
-    },
-    delayRun: function(time){
-        var self = this;
-        setTimeout(function(){
-            self.run();
-        }, time || 100);
-    },
-    run: function(){
-        // debug('run');
-        this.timer = setInterval(function(self) {
-            if (++self.count > self.maxcount || self.move())
-                clearInterval(self.timer);
-        }, this.interval, this);
-    },
-    move: function(){
-        var i = 0;
-        while (i < this.buttons.length){
-            var info = this.buttons[i];
-            var button = $(info.id);
-            if(button){
-                if(info.clone === true){
-                    button = button.cloneNode(true);
-                }
-                let ins;
+        // // 改 User Agent Overrider 按钮为右键弹出菜单
+        // {id: "useragentoverrider-button",
+        //     attr: {
+        //         type: "",
+        //         context: "_child"
+        //     }
+        // },
 
-                if (info.insertBefore && (ins = $(info.insertBefore))){
-                    ins.parentNode.insertBefore(button, ins);
+        // // 增加 Stylish 右键弹出菜单功能
+        // {id: "stylish-toolbar-button",
+        //     attr: {
+        //         context: "stylish-popup"
+        //     }
+        // },
+    ];
 
+    if (window.MyMoveButton) {
+        try {
+            window.MyMoveButton.unint();
+            delete window.MyMoveButton;
+        } catch(e) {
+            window.MyMoveButton = null;
+        }
+    }
+
+    window.MyMoveButton = {
+        buttons: [],
+        interval: 200, // 200毫秒间隔
+        maxcount: 100, // 最大100回，至少 200 * 100 毫秒
+        count: 0,
+        timer: null,
+
+        init: function() {
+            this.delayRun(0);
+
+            let menuitem = $C('menuitem', {
+                id: "uc-movebutton",
+                label: "Movebutton 重新运行",
+                tooltiptext: "重新按照脚本内的设置移动按钮的位置",
+                oncommand: "MyMoveButton.delayRun();"
+            });
+
+            let ins = $('devToolsSeparator');
+            ins.parentNode.insertBefore(menuitem, ins);
+
+            // window.addEventListener('aftercustomization', this, false);
+        },
+        unint: function() {
+            // window.removeEventListener('aftercustomization', this, false);
+        },
+        handleEvent: function(event) {
+            switch (event.type) {
+                case 'aftercustomization':
+                    // debug('aftercustomization');
+                    this.delayRun();
+                    break;
+            }
+        },
+        delayRun: function(time) {
+            this.reset();
+
+            var self = this;
+            setTimeout(function() {
+                self.run();
+            }, time || 100);
+        },
+        reset: function() {
+            this.buttons = buttons.slice(0); // 克隆一份
+            this.count = 0;
+            this.timer = null;
+        },
+        run: function() {
+            if (this.buttons.length == 0) return;
+
+            this.timer = setInterval(function(self) {
+                if (++self.count > self.maxcount || self.move())
+                    clearInterval(self.timer);
+            }, this.interval, this);
+        },
+        move: function() {
+            var i = 0,
+                info, success;
+            while (i < this.buttons.length) {
+                info = this.buttons[i];
+                success = this.moveOneButton(info);
+                if (success) {
                     this.buttons.splice(i, 1);
-                    continue;
                 }
+                i++;
+            }
+            return this.buttons.length === 0 ? true : false;
+        },
+        moveOneButton: function(info) {
+            var button = $(info.id) || document.querySelector(info.id);
+            // debug('check button id: ' + info.id);
+            if (!button) return false;
 
-                if (info.insertAfter && (ins = $(info.insertAfter))){
-                    ins.parentNode.insertBefore(button, ins.nextSibling);
+            if (info.clone === true) {
+                button = button.cloneNode(true);
+            }
 
-                    this.buttons.splice(i, 1);
-                    continue;
-                }
-
-                if (info.pos && (parseInt(info.pos, 10) > 0)){
-                    let bar = $(info.bar) || button.parentNode;
-                    ins = bar.children[parseInt(info.pos, 10) - 1];
-                    if(ins){
-                        bar.insertBefore(button, ins);
-                    }else{
-                        bar.appendChild(button);
-                    }
-
-                    this.buttons.splice(i, 1);
-                    continue;
+            // 先设置属性
+            if (info.attr) {
+                for (let [key, val] in Iterator(info.attr)) {
+                    if (typeof val == "function")
+                        info.attr[key] = val = "(" + val.toSource() + ").call(this, event);";
+                    button.setAttribute(key, val);
                 }
             }
-            i++;
-        }
-        return this.buttons.length === 0 ? true : false;
+
+            // 移动
+            let ins;
+            if (info.insertBefore && (ins = $(info.insertBefore))) {
+                ins.parentNode.insertBefore(button, ins);
+            } else if (info.insertAfter && (ins = $(info.insertAfter))) {
+                ins.parentNode.insertBefore(button, ins.nextSibling);
+            } else if (info.bar) {
+                let bar = $(info.bar) || button.parentNode;
+                ins = bar.children[parseInt(info.pos, 10) - 1];
+                if (ins) {
+                    bar.insertBefore(button, ins);
+                } else {
+                    bar.appendChild(button);
+                }
+            } else {
+                return false;
+            }
+
+            return true;
+        },
+    };
+
+    MyMoveButton.init();
+
+
+    function debug() { Application.console.log('[MyMoveButton DEBUG] ' + Array.slice(arguments)); }
+    function $(id) { return document.getElementById(id); }
+    function $A(args) { return Array.prototype.slice(args); }
+    function $C(name, attr) {
+        var el = document.createElement(name);
+        if (attr) Object.keys(attr).forEach(function(n) el.setAttribute(n, attr[n]));
+        return el;
     }
-};
-
-moveButton.init();
-
-function debug() { Application.console.log('[moveButton DEBUG] ' + Array.slice(arguments)); }
-function $(id) { return document.getElementById(id); }
-function $A(args) { return Array.prototype.slice(args); }
-
 })();
