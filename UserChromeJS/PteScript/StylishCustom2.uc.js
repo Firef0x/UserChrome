c2963c834c9438fcd2bcf19b6aa69b4a4d82f8e3
Firef0x <Firefgx@gmail.com>
2015-02-17 10:17:41 +0800

    更新StylishCustom2
    从 http://pan.baidu.com/share/link?uk=2467242534&shareid=3781338190
    处更新 StylishCustom2.uc.js 版本为 0.0.2.20150205

--- UserChromeJS/PteScript/StylishCustom2.uc.js
+++ UserChromeJS/PteScript/StylishCustom2.uc.js
@@ -4,7 +4,7 @@
 // @namespace    stylishCustom2@uc.js
 // @author       Crab
 // @include      about:stylish-edit*
-// @version      0.0.1.20150114
+// @version      0.0.2.20150205
 // ==/UserScript==
 if(location.href.indexOf('about:stylish-edit') == 0) {
 /* 
@@ -18,6 +18,9 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 // 6.显示行和列，在行文本框内输入正整数回车可跳转之对应行
 */
 	var stylishCustom2 = {
+
+		_revertOldFindbar: false, //还原旧查找栏(默认关闭，若开启请将 false , 改为 true);
+
 		insertRules: {
 			//可以在text中按格式加入一些常用的属性或文本
 			text: [
@@ -25,7 +28,12 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 				'-moz-box-ordinal-group:',
 				'-moz-linear-gradient',
 				'vertical-align:middle',
-				'text-decoration:underline'],
+				'text-decoration:underline',
+				['::before 伪元素', '::before {\n\tcontent: ""\n}', 3], // 插入模板块数组，
+				['::after 伪元素', '::after {\n\tcontent: ""\n}', 3],	// 参数 0：菜单名，
+																		// 参数 1：插入内容，其中\n代表换行符，\t为制表符（tab）。
+																		// 参数 2：插入内容后光标所在对应内容倒数位置，可忽略默认为0，即最末
+			],
 			domRules: {
 				'url("")': '@-moz-document url(""){\n\n}',
 				'url-prefix("")': '@-moz-document url-prefix(""){\n\n}',
@@ -33,7 +41,9 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 				'regexp("")': '@-moz-document regexp(""){\n\n}'
 			}
 		},
+
 		locale : ['zh-CN', 'en-US'].indexOf(Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch).getCharPref('general.useragent.locale')),
+
 		_localeText: {
 			unperview: ['取消预览', 'UnPerview'],
 			lineNumber: ['行: ', 'Ln: '],
@@ -45,9 +55,11 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 			documentRules:['文档规则', 'Document Rule'],
 			chromeMenu: ['Chrome 路径' , 'Chrome URL'],
 		},
+
 		localeText: function(name){
 			return this._localeText[name][this.locale == -1 ? 1 : this.locale];
 		},
+
 		oldPreview: null,
 		init: function () {
 			if(document.getElementById('stylishCustomToolbar') || !sourceEditor || sourceEditorType == 'textarea') return;
@@ -57,15 +69,17 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 			this.setToolButtons();
 			this.setShortcuts();
 			this.oldPreview = codeElementWrapper.value;
+			this.revertOldFindbar();
 			sourceEditor.on('cursorActivity', this.setLineAndcolNum.bind(this));
 		},
+
 		setToolButtons: function () {
 			var _et = document.getElementById('editor-tools'),
 				cE = this.createElement,
 				editortools = cE('hbox', {id: 'stylishCustomToolbar'}, [_et.parentNode, _et.nextSibling]),
 				insertMenupopup =  document.getElementById('insert-data-uri').parentNode;
 			//工具栏按钮
-			cE('button', {id: 'comment', class: 'devtools-toolbarbutton', label: this.localeText('comment'), onclick: this.setComment.bind(this)}, editortools);
+			cE('button', {id: 'comment', class: 'devtools-toolbarbutton', label: this.localeText('comment'), onclick: this.setComment}, editortools);
 			this.unperview = cE('button', {id: 'unperview', class: 'devtools-toolbarbutton', label: this.localeText('unperview'), onclick: this.unperview.bind(this)}, editortools);
 			cE('spacer', {flex: '1'}, editortools);
 			cE('label', {style: 'max-height: 20px; margin:7px 0 4px 0;', value: this.localeText('lineNumber')}, editortools);
@@ -78,22 +92,24 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 			var insertTextMenupopup = cE('menupopup', {}, 
 					cE('menu', {id:'insertTextMenu',  label: this.localeText('insertText')}, insertMenupopup));
 			//插入文档规则
-			var documentRules = cE('menupopup', {},cE('menu', {id:'documentRules',  label: this.localeText('documentRules')}, insertTextMenupopup));
+			var documentRules = cE('menupopup', {}, cE('menu', {id:'documentRules', label: this.localeText('documentRules')}, insertTextMenupopup));
 			var {text, domRules} = this.insertRules;
 			for(var i in domRules){
-				cE('menuitem', {label: i, value: domRules[i], onclick: 'stylishCustom2.insertString(this.value, 6)'}, documentRules);
+				cE('menuitem', {label: i, onclick: this.insertString.bind(null, domRules[i], 6)}, documentRules);
 			}
 			cE('menuseparator', {}, insertTextMenupopup);
 			for(var i in text){
-				cE('menuitem', {label: text[i], value: text[i], onclick: 'stylishCustom2.insertString(this.value)'}, insertTextMenupopup);
+				cE('menuitem', {label: Array.isArray(text[i]) ? text[i][0] : text[i], onclick: this.insertString.bind(null, text[i])}, insertTextMenupopup);
 			}
 
 		},
+
 		setLineAndcolNum: function(){
 			var {line, ch} = sourceEditor.getCursor();
 			this.lineNumber.value = line + 1;
 			this.colNumber.value = this.localeText('colNumber') + ch;
 		},
+
 		goToLine: function(event){
 			if(event.keyCode == 13 || event.keyCode == 108){
 				event.preventDefault();
@@ -103,6 +119,7 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 				sourceEditor.focus();
 			}
 		},
+
 		unperview: function(){
 			style.name = nameE.value;
 			style.code = stylishCustom2.oldPreview;
@@ -113,25 +130,11 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 				style.setPreview(true);
 			}, 50);
 		},
-		getSelectionRange: function(){
-			var gs = sourceEditor.getCursor(),
-				ge = sourceEditor.getCursor('end'),
-				from = {};
-			if(gs.line == ge.line && gs.ch == ge.ch && sourceEditor.somethingSelected()) {
-				let sa = sourceEditor.getSelection().split('\n'),
-					sal = sa.length - 1;
-				from.line = ge.line - sal;
-				let vl = sourceEditor.getText().split('\n')[from.line].length;
-				from.ch = Math.max(sal==0 ? (ge.ch - sa[0].length) : (vl - sa[0].length), 0);
-			}else{
-				from = gs;
-			}
-			return [from, ge];
-		},
+
 		setComment: function(){
 			var selText = sourceEditor.getSelection();
 			if(!selText) return;
-			var [from, to] = this.getSelectionRange(),
+			var [from, to] = [sourceEditor.getCursor('start'), sourceEditor.getCursor('end')],
 				re = /(^[\W\s]*?\/\*)((?:(?!\/\*|\*\/)[\s\S])*?)(\*\/[\W\s]*?$)/;
 			if(!re.test(selText)){
 				if(!/\/\*|\*\//.test(selText)){
@@ -145,17 +148,38 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 			}
 			sourceEditor.focus();
 		},
+
 		setShortcuts: function(){
 			sourceEditor.config.extraKeys['Shift-1'] = function(e){
 				sourceEditor.replaceSelection(' !important;');
 			};
 			sourceEditor.config.extraKeys['Ctrl-/'] = this.setComment.bind(this);
 		},
+
 		insertString: function (str, range) {
-			sourceEditor.replaceSelection(str);
+			if(Array.isArray(str)){
+				sourceEditor.replaceSelection(str[1]);
+				range = str[2] || range || 0;
+			}else{
+				sourceEditor.replaceSelection(str);
+			}
 			if(range)
 				sourceEditor.setCursor(sourceEditor.getPosition(sourceEditor.getOffset(sourceEditor.getCursor('end')) - range));
 			sourceEditor.focus();
+		},
+
+		revertOldFindbar: function(){
+			if(!this._revertOldFindbar) return;
+			Object.defineProperty(codeElementWrapper, 'scrollElement', {get: function(){
+				if(sourceEditorType == 'sourceeditor')
+					return sourceEditor.container.contentDocument.getElementsByClassName('CodeMirror-code')[0];
+				return sourceEditor.inputField;
+			}});
+			finder.requestMatchesCount = function(){};
+			var findBar = document.getElementById('findbar');
+			Object.defineProperty(findBar.browser, '_lastSearchString', {get: function() finder.searchString});
+			findBar.browser.finder = finder;
+			findBar.open();
 		},
 
 		showDocumentList: function (event, isChrome) {
@@ -192,6 +216,7 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 				}
 			}
 		},
+
 		appendContainedDocuments: function (array, docShell, type) {
 			var containedDocShells = docShell.getDocShellEnumerator(type,
 			Components.interfaces.nsIDocShell.ENUMERATE_FORWARDS);
@@ -212,17 +237,19 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 				}
 			}
 		},
+
 		emptyChildren: function (node) {
 			while (node.hasChildNodes()) {
 				node.removeChild(node.lastChild);
 			}
 		},
+
 		createElement: function(name, attr, parent){
 			var e = document.createElementNS(
 					'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', name);
 			if(attr){
 				for (var i in attr) {
-					if(typeof attr[i] == 'function' || (i == 'value' && name == 'textbox'))
+					if(typeof attr[i] == 'function' || (i == 'value' && (name == 'textbox' || name == 'menuitem')))
 						e[i] = attr[i];
 					else
 						e.setAttribute(i, attr[i]);
@@ -237,6 +264,7 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 			}
 			return e;
 		},
+
 		addMenuItem: function (parent, doc) {
 			this.createElement('menuitem', {
 				label: doc.title || doc.location.href, 
@@ -250,6 +278,7 @@ if(location.href.indexOf('about:stylish-edit') == 0) {
 				}
 			}, parent);
 		},
+
 		closeMenus: function (node) {
 			if ('tagName' in node) {
 				if (node.namespaceURI == 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul' && (node.tagName == 'menupopup' || node.tagName == 'popup')) node.hidePopup();
