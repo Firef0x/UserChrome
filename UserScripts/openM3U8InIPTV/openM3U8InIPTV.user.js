a37f9becc5170caacb848260d1251c7ad80362be
Firef0x <Firefgx@gmail.com>
2019-10-31 01:13:50 +0800

    Add user script openM3U8InIPTV
    增加用户脚本 openM3U8InIPTV
    
    Signed-off-by: Firef0x <Firefgx@gmail.com>

---
+++ UserScripts/openM3U8InIPTV/openM3U8InIPTV.user.js
@@ -0,0 +1,23 @@
+﻿// ==UserScript==
+// @name           openM3U8InIPTV
+// @version        0.0.1
+// @author         Firef0x
+// @description    Open M3U8 play list in IPTV page
+// @include        /^https?:\/\/[^.]*\.iptv444\.com/
+// @require        http://code.jquery.com/jquery-1.9.1.min.js
+// @run-at         document-idle
+// @namespace      https://greasyfork.org/users/999999
+// ==/UserScript==
+(function() {
+  "use strict";
+
+  $(function() {
+    // 获取播放器（video）对象
+    var video = $("video");
+    var video_url = null;
+    if (video) {
+      video_url = video.attr("src");
+      window.open(video_url, "_top");
+    }
+  });
+})();
